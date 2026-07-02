69f033fefa4e0bcdefb241fc62a961926aaaadad
diana.copacondori <cvdianacarolina26@gmail.com>
2026-07-01 21:30:54 -0500

    ofertas y pedidos

--- main_Page/script.js
+++ main_Page/script.js
@@ -13,15 +13,17 @@ if (modoGuardado === "oscuro") {
 if (interruptor) {
   interruptor.checked = modoGuardado === "oscuro";
 
-  interruptor.addEventListener("change", () => {
-    if (interruptor.checked) {
-      document.body.classList.add("dark-mode");
-      localStorage.setItem("modo", "oscuro");
-    } else {
-      document.body.classList.remove("dark-mode");
-      localStorage.setItem("modo", "claro");
+  interruptor.addEventListener("change",()=>{
+
+    if(interruptor.checked){
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("modo","oscuro");
+    }else{
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("modo","claro");
     }
-  });
+    crearDestellos();
+});
 }
 
 // Menú hamburguesa seguro para todas las páginas
@@ -37,4 +39,34 @@ if (hamburger && navLinks) {
       hamburger.classList.remove("active");
     });
   });
+}
+/*destellos en el slider de temas*/
+function crearDestellos() {
+
+    const slider = document.querySelector(".theme-slider");
+
+    const rect = slider.getBoundingClientRect();
+
+    for(let i = 0; i < 12; i++){
+
+        const estrella = document.createElement("div");
+
+        estrella.classList.add("sparkle");
+
+        estrella.style.left =
+            rect.left + rect.width/2 + (Math.random()*80-40) + "px";
+
+        estrella.style.top =
+            rect.top + rect.height/2 + (Math.random()*60-30) + "px";
+
+        document.body.appendChild(estrella);
+
+        setTimeout(() => {
+
+            estrella.remove();
+
+        },800);
+
+    }
+
 }
